a6bb0a4cdf48cb7cd8e5d1a434abd6f0d9719e0c
zakaria_ali <zak95469@gmail.com>
2026-02-15 10:36:13 +0200

    Fix 500 error: Switch to non-manifest static files storage for stability

--- alhilal_project/settings.py
+++ alhilal_project/settings.py
@@ -155,7 +155,7 @@ LOCALE_PATHS = (
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
